376d810b5289d8458dee81a231986472fb4b4d35
Philipp Muhr <philipp.muhr@stud.th-deg.de>
2020-09-05 17:07:48 +0200

    add some more buttons; change css style

--- RaspberryWebserver/JavaScript/PlaySound.js
+++ RaspberryWebserver/JavaScript/PlaySound.js
@@ -1,5 +1,28 @@
-function test(){
-
-    alert("TEST");
-
+function playSoundOne(){
+    var audio = new Audio('../src/Mouse-Click.mp3');
+    audio.play(); 
 }
+
+function playSoundTwo(){
+    var audio = new Audio('../src/Mouse-Click.mp3');
+    audio.play();
+}
+
+function playSoundThree(){
+    var audio = new Audio('../src/Mouse-Click.mp3');
+    audio.play();
+}
+
+function playSoundFour(){
+    var audio = new Audio('../src/Mouse-Click.mp3');
+    audio.play();
+    
+}
+
+
+$(document).ready(function(){
+    $("#soundOne").click(playSoundOne);
+    $("#soundTwo").click(playSoundTwo);
+    $("#soundThree").click(playSoundThree);
+    $("#soundFour").click(playSoundFour);
+  });
