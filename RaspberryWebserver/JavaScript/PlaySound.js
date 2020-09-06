326581bdb4ce95667bce9ae8dc236d9ae11ae186
Philipp Muhr <philipp.muhr@stud.th-deg.de>
2020-09-06 16:19:03 +0200

    Change click listener is now called in html document

--- RaspberryWebserver/JavaScript/PlaySound.js
+++ RaspberryWebserver/JavaScript/PlaySound.js
@@ -1,28 +1,4 @@
-function playSoundOne(){
-    var audio = new Audio('../src/sample_1.m4a');
+function playSoundSample(path){
+    var audio = new Audio(path);
     audio.play(); 
 }
-
-function playSoundTwo(){
-    var audio = new Audio('../src/sample_2.m4a');
-    audio.play();
-}
-
-function playSoundThree(){
-    var audio = new Audio('../src/sample_3.m4a');
-    audio.play();
-}
-
-function playSoundFour(){
-    var audio = new Audio('../src/sample_4.m4a');
-    audio.play();
-    
-}
-
-
-$(document).ready(function(){
-    $("#soundOne").click(playSoundOne);
-    $("#soundTwo").click(playSoundTwo);
-    $("#soundThree").click(playSoundThree);
-    $("#soundFour").click(playSoundFour);
-  });
